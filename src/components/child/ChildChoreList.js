999fed38ac146618dbc8e8f286f2f4975815c82c
Devin-Bennett <devincbennett@gmail.com>
2019-09-18 20:27:42 -0600

    filtering the child chores

--- src/components/child/ChildChoreList.js
+++ src/components/child/ChildChoreList.js
@@ -56,13 +56,51 @@ class ChildChoreList extends Component {
 const mapStateToProps = state => {
   console.log("state.auth.activeUser.name", state.auth.activeUser.name);
 
-  const childChores = _.map(state.chores, (val, cid) => {
-    // i could do the filtering for completed, in progress, and submitted chores in here.
-    console.log("val: ", val.child);
+  // const childChores = _.map(state.chores, (val, cid) => {
+  //   // i could do the filtering for completed, in progress, and submitted chores in here.
+  //   console.log("val: ", val.child);
+  //   console.log("val: ", val);
+  //   if (val.status === "Rework") {
+  //     return { ...val, cid };
+  //   }
+  // });
+
+  console.log("filteredChores: ", filteredChores);
+
+  const chores = _.map(state.chores, (val, cid) => {
     return { ...val, cid };
   });
 
-  return { childChores: childChores, activeUser: state.auth.activeUser };
+  const filteredChores = _.filter(chores, function(e) {
+    return e.status === "Rework";
+  });
+  // const filteredChores = _.map(childChores, val => {
+  //   // i could do the filtering for completed, in progress, and submitted chores in here.
+  //   // can i check to see if it's undefined before I run it through this. I think I can check for null
+  //   // or maybe i can use the .filter function either lodash or regular javascript
+  // or maybe I can in the render and not in map state to props.
+  //   console.log("filtered chores val: ", val);
+  //   if (val != null) {
+  //     if (val.status === "Rework") {
+  //       return {
+  //         childName: val.childName,
+  //         choreName: val.choreName,
+  //         cid: val.cid,
+  //         day: val.day,
+  //         description: val.description,
+  //         pointsValue: val.pointsValue,
+  //         rejectReason: val.rejectReason,
+  //         status: val.status
+  //       };
+  //     }
+  //   }
+  // });
+
+  // console.log("child chores after filter: ", childChores);
+
+  // const evenNumbers = _.filter(state.chores, function(e){ return e.status === "Rework"    ;
+
+  return { childChores: filteredChores, activeUser: state.auth.activeUser };
 };
 export default connect(
   mapStateToProps,
