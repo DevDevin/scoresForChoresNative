496f9b45037ca8022a5d3dd507062428f0d19fd0
Devin-Bennett <devincbennett@gmail.com>
2019-09-26 20:53:03 -0600

    afdafd

---
+++ src/components/child/ChildRewardRequestsList.js
@@ -0,0 +1,88 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Actions } from "react-native-router-flux";
+import { FlatList, View, Picker } from "react-native";
+import { rewardRequestsFetch } from "../../actions/ParentActions";
+import ChildRewardRequestsListItem from "./ChildRewardRequestsListItem";
+import { usersFetch } from "../../actions/AuthActions";
+
+class ChildRewardRequestList extends Component {
+  state = {
+    child: "All"
+  };
+
+  componentWillMount() {
+    this.props.rewardRequestsFetch();
+    this.props.usersFetch();
+  }
+
+  onButtonPress() {
+    console.log("addUserPress");
+    Actions.userCreate();
+  }
+
+  render() {
+    console.log("inside render");
+    const rewardRequests = this.props.rewardRequests;
+    const users = this.props.users;
+    const children = _.filter(users, function(item) {
+      return item.status === "child";
+    });
+
+    child = this.state.child;
+
+    let filteredRequests;
+    if (child === "All") {
+      console.log("inside if: ", child);
+      filteredRequests = rewardRequests;
+    } else {
+      filteredRequests = _.filter(rewardRequests, function(item) {
+        console.log("inside else: ", child);
+        return item.childName === child;
+      });
+    }
+
+    return (
+      <View>
+        <Picker
+          selectedValue={this.state.child}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ child: itemValue })
+          }
+        >
+          <Picker.Item label="All" value="All" />
+          {children.map(function(child) {
+            return <Picker.Item label={child.name} value={child.name} />;
+          })}
+        </Picker>
+
+        <FlatList
+          data={filteredRequests}
+          renderItem={({ item }) => (
+            <ChildRewardRequestsListItem Item rewardRequest={item} />
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const rewardRequests = _.map(state.rewardRequests, (val, rid) => {
+    return { ...val, rid };
+  });
+
+  ///bring in users
+  const users = _.map(state.users, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { rewardRequests: rewardRequests, users: users };
+};
+
+export default connect(
+  mapStateToProps,
+  { rewardRequestsFetch, usersFetch }
+)(ChildRewardRequestList);
---
+++ src/components/child/ChildRewardRequestsListItem.js
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { Actions } from "react-native-router-flux";
+import {
+  rewardRequestsFetch,
+  rewardRequestAccept,
+  rewardRequestReject
+} from "../../actions/ParentActions";
+
+class ChildRewardRequestsListItem extends Component {
+  state = {
+    isModalVisible: false
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
+  onRowPress(activeUser) {
+    // actions.something
+  }
+
+  onAccept(childName, uid, pointsValue, rid, rewardName) {
+    console.log("uid: ", uid);
+    console.log("childName: ", childName);
+    this.props.rewardRequestAccept(childName, pointsValue, rid, rewardName);
+  }
+  onReject(childName, uid, pointsValue, rid, rewardName) {
+    this.props.rewardRequestReject(
+      childName,
+      uid,
+      pointsValue,
+      rid,
+      rewardName
+    );
+  }
+
+  render() {
+    const childName = this.props.rewardRequest.childName;
+    const pointsValue = this.props.rewardRequest.pointsValue;
+    const uid = this.props.rewardRequest.uid;
+    const rid = this.props.rewardRequest.rid;
+    const rewardName = this.props.rewardRequest.rewardName;
+    console.log("rewardName: ", this.props.rewardRequest);
+
+    return (
+      <View style={{ flex: 1 }}>
+        <TouchableWithoutFeedback onPress={this.toggleModal}>
+          <View style={styles.childStyle}>
+            <View style={styles.choreStyle}>
+              <Text style={styles.choreNameStyle}>{childName}</Text>
+              <Text style={styles.choreInfoStyle}>{pointsValue}</Text>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <TouchableOpacity
+                  onPress={this.onAccept.bind(
+                    this,
+                    childName,
+                    uid,
+                    pointsValue,
+                    rid,
+                    rewardName
+                  )}
+                  style={styles.buttonStyle}
+                >
+                  <Text style={styles.textStyle}>Accept</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={this.onReject.bind(
+                    this,
+                    childName,
+                    uid,
+                    pointsValue,
+                    rid,
+                    rewardName
+                  )}
+                  style={styles.buttonStyle}
+                >
+                  <Text style={styles.textStyle}>Reject</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  },
+  choreNameStyle: {
+    fontSize: 26,
+    paddingLeft: 15,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold"
+  },
+  childStyle: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "#ddd",
+    borderBottomWidth: 0,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    backgroundColor: "powderblue",
+    width: Dimensions.get("window").width
+  },
+  choreInfoStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  choreStyle: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "#d67d72",
+    alignItems: "center",
+    borderColor: "#ddd"
+  },
+  modalTextStyle: {
+    fontSize: 24,
+    paddingLeft: 5
+  },
+  buttonStyle: {
+    width: 100,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#007aff",
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: "center",
+    fontSize: 16,
+    fontWeight: "600",
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    activeUser: state.auth.activeUser
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { rewardRequestsFetch, rewardRequestAccept, rewardRequestReject }
+)(ChildRewardRequestsListItem);
