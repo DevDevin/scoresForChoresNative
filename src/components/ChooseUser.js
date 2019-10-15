75838751794b16ba49f1f5bd99353115a1fa792c
Devin-Bennett <devincbennett@gmail.com>
2019-10-14 20:12:58 -0600

    action button stuff

--- src/components/ChooseUser.js
+++ src/components/ChooseUser.js
@@ -2,8 +2,9 @@ import _ from "lodash";
 import React, { Component } from "react";
 import { Actions } from "react-native-router-flux";
 import { connect } from "react-redux";
-import { View, FlatList, ActivityIndicator } from "react-native";
+import { View, FlatList, ScrollView } from "react-native";
 import { usersFetch, loadingUsersEnd } from "../actions/AuthActions";
+import { FloatingAction } from "react-native-floating-action";
 import ActionButton from "react-native-action-button";
 import UserListItem from "./UserListItem";
 import Spinner from "react-native-loading-spinner-overlay";
@@ -48,25 +49,43 @@ class ChooseUser extends Component {
     return (
       <View style={{ flex: 1 }}>
         {this.renderSpinner()}
-        <View>
-          <FlatList
-            data={users}
-            renderItem={({ item }) => <UserListItem user={item} />}
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "flex-end",
-            marginBottom: 36
-            // backgroundColor: "grey"
-          }}
-        >
-          <ActionButton
-            buttonColor="rgba(231,76,60,1)"
-            onPress={this.onButtonPress.bind(this)}
-          />
-        </View>
+        <ScrollView>
+          <View>
+            <FlatList
+              data={users}
+              renderItem={({ item }) => <UserListItem user={item} />}
+            />
+            <FloatingAction
+              // actions={actions}
+              onOpen={this.onButtonPress.bind(this)}
+            />
+            {/* <View
+              style={{
+                flex: 1,
+                justifyContent: "flex-end",
+                // // zIndex: 999,
+                marginBottom: 80,
+                // position: "absolute"
+                // // // backgroundColor: "grey"
+                // width: 60,
+                // height: 60,
+                // borderRadius: 30,
+                // backgroundColor: "#ee6e73",
+                // position: "absolute",
+                // bottom: 10,
+                // right: 10
+                resizeMode: "contain",
+                width: 50,
+                height: 50
+              }}
+            >
+              <ActionButton
+                buttonColor="rgba(231,76,60,1)"
+                onPress={this.onButtonPress.bind(this)}
+              />
+            </View> */}
+          </View>
+        </ScrollView>
       </View>
     );
   }
